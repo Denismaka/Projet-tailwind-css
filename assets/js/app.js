a1f150023e19daf84893231aabfdc71e9e1a6e0e
Denismaka <makadenis370@gmail.com>
2025-05-16 22:00:30 +0100

    mise en place du script scroolReveal

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,32 @@ menuButton.addEventListener("click", () => {
 
 // Script pour l'année dynamique
 document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("backToTop");
+
+    // Gestion du clic
+    btn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+
+    // Apparition conditionnelle
+    window.addEventListener("scroll", () => {
+        btn.classList.toggle("opacity-0", window.scrollY < 300);
+        btn.classList.toggle("invisible", window.scrollY < 300);
+        btn.classList.toggle("opacity-100", window.scrollY >= 300);
+    });
+});
+
+// Librairies ScrollReveal pour la gestion du scrool
+ScrollReveal;
+ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+});
+ScrollReveal().reveal(".container, .heading", { origin: "top" });
+ScrollReveal().reveal(".col-md-4, form", { origin: "left" });
+ScrollReveal().reveal("h1, h3, h4, h5, h2", { origin: "bottom" });
+ScrollReveal().reveal("p, .row, button", { origin: "left" });
